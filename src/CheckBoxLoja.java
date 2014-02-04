a84ad3ca1a3bb3d12fa6c73eefcbae43275aac4c
samuelsp <samp_81@yahoo.com.br>
2014-02-04 18:32:16 -0200

    upload 04/02/2014

---
+++ src/CheckBoxLoja.java
@@ -0,0 +1,17 @@
+
+public class CheckBoxLoja {
+	
+	public static void main(String[] args) {
+		String checkBoxLoja = "checkBoxLoja";		
+		
+		int i = 1;	
+		
+		while(i <= 4){
+			System.out.println(checkBoxLoja+i+"Saida");
+			i  += 1;
+		}
+		
+	}
+	
+
+}
